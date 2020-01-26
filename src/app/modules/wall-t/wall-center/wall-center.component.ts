f9488d759044245d7a22f996fbfc5d4b47e32778
nildiert <niljordan23@gmail.com>
2020-01-26 00:22:17 -0500

    Change name of variable

--- src/app/modules/wall-t/wall-center/wall-center.component.ts
+++ src/app/modules/wall-t/wall-center/wall-center.component.ts
@@ -34,7 +34,7 @@ export class WallCenterComponent implements OnInit {
 
     dialogRef.afterClosed().subscribe(result => {
       console.log('The dialog was closed');
-      this.animal = result;
+      this.dialog = result;
     });
   }
 
